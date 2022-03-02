269e2b3eef5ef5fe1e8be1ef2a002fbde6e8e5fc
Tiffauy <Thoeung5@gmail.com>
2022-03-01 20:11:08 -0500

    Changed Recipe class, changed ListRecipeFragment to match.

--- app/src/main/java/tiffany/hoeung/recipeapp/ListRecipeFragment.java
+++ app/src/main/java/tiffany/hoeung/recipeapp/ListRecipeFragment.java
@@ -1,5 +1,6 @@
 package tiffany.hoeung.recipeapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -9,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,20 +25,23 @@ public class ListRecipeFragment extends Fragment implements RecyclerAdapter.OnNo
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        //get layout manager (aka, what format RV should take)
         layoutManager = new GridLayoutManager(this.getActivity(), 2);
         recipeRV.setLayoutManager(layoutManager);
-        //recipeRVRight.setLayoutManager(layoutManager);
-
+        // adds recipes to recipeList arraylist
         createInitialRecipeList();
-
+        //create a new recycler view adapter
         adapterRV = new RecyclerAdapter(recipeList, this);
-
+        // set the recycler view's adapter
         recipeRV.setAdapter(adapterRV);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get favorites button
+        // Button favButton =
     }
 
     @Override
@@ -44,7 +50,7 @@ public class ListRecipeFragment extends Fragment implements RecyclerAdapter.OnNo
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list_recipe, container, false);
 
-        recipeRV = view.findViewById(R.id.recipeRV1);
+        recipeRV = view.findViewById(R.id.recipeRV);
 
         return view;
     }
@@ -52,25 +58,28 @@ public class ListRecipeFragment extends Fragment implements RecyclerAdapter.OnNo
     @Override
     public void onNoteClick(int position) {
         System.out.println("position " + position + " pressed");
+        Toast.makeText(this.getActivity(), "Position " + position + " pressed\n" + recipeList.get(position).recipeName, Toast.LENGTH_SHORT).show();
         // this is where we switch to the recipe screen
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     public void createInitialRecipeList() {
         System.out.println("Creating initial recipes!");
 
-        recipeList.add(new Recipe(recipeList.size(), "Spaghetti and meatballs", getContext().getDrawable(R.drawable.spaghetti)));
-        recipeList.add(new Recipe(recipeList.size(), "Cottage Pie", getContext().getDrawable(R.drawable.cottage_pie)));
-        recipeList.add(new Recipe(recipeList.size(), "Crab Rangoon", getContext().getDrawable(R.drawable.crab_rangoon)));
-        recipeList.add(new Recipe(recipeList.size(), "Zaru Soba", getContext().getDrawable(R.drawable.zaru_soba)));
-        recipeList.add(new Recipe(recipeList.size(), "Chicken Enchiladas", getContext().getDrawable(R.drawable.chicken_enchiladas)));        recipeList.add(new Recipe(recipeList.size(), "Spaghetti and meatballs", getContext().getDrawable(R.drawable.spaghetti)));
-        recipeList.add(new Recipe(recipeList.size(), "Cottage Pie", getContext().getDrawable(R.drawable.cottage_pie)));
-        recipeList.add(new Recipe(recipeList.size(), "Crab Rangoon", getContext().getDrawable(R.drawable.crab_rangoon)));
-        recipeList.add(new Recipe(recipeList.size(), "Zaru Soba", getContext().getDrawable(R.drawable.zaru_soba)));
-        recipeList.add(new Recipe(recipeList.size(), "Chicken Enchiladas", getContext().getDrawable(R.drawable.chicken_enchiladas)));
-        recipeList.add(new Recipe(recipeList.size(), "Spaghetti and meatballs", getContext().getDrawable(R.drawable.spaghetti)));
-        recipeList.add(new Recipe(recipeList.size(), "Cottage Pie", getContext().getDrawable(R.drawable.cottage_pie)));
-        recipeList.add(new Recipe(recipeList.size(), "Crab Rangoon", getContext().getDrawable(R.drawable.crab_rangoon)));
-        recipeList.add(new Recipe(recipeList.size(), "Zaru Soba", getContext().getDrawable(R.drawable.zaru_soba)));
-        recipeList.add(new Recipe(recipeList.size(), "Chicken Enchiladas", getContext().getDrawable(R.drawable.chicken_enchiladas)));
+        recipeList.add(new Recipe("Spaghetti and meatballs", getContext().getDrawable(R.drawable.spaghetti)));
+        recipeList.add(new Recipe("Cottage Pie", getContext().getDrawable(R.drawable.cottage_pie)));
+        recipeList.add(new Recipe("Crab Rangoon", getContext().getDrawable(R.drawable.crab_rangoon)));
+        recipeList.add(new Recipe("Zaru Soba", getContext().getDrawable(R.drawable.zaru_soba)));
+        recipeList.add(new Recipe("Chicken Enchiladas", getContext().getDrawable(R.drawable.chicken_enchiladas)));
+        recipeList.add(new Recipe("Spaghetti and meatballs", getContext().getDrawable(R.drawable.spaghetti)));
+        recipeList.add(new Recipe("Cottage Pie", getContext().getDrawable(R.drawable.cottage_pie)));
+        recipeList.add(new Recipe("Crab Rangoon", getContext().getDrawable(R.drawable.crab_rangoon)));
+        recipeList.add(new Recipe("Zaru Soba", getContext().getDrawable(R.drawable.zaru_soba)));
+        recipeList.add(new Recipe("Chicken Enchiladas", getContext().getDrawable(R.drawable.chicken_enchiladas)));
+        recipeList.add(new Recipe("Spaghetti and meatballs", getContext().getDrawable(R.drawable.spaghetti)));
+        recipeList.add(new Recipe("Cottage Pie", getContext().getDrawable(R.drawable.cottage_pie)));
+        recipeList.add(new Recipe("Crab Rangoon", getContext().getDrawable(R.drawable.crab_rangoon)));
+        recipeList.add(new Recipe("Zaru Soba", getContext().getDrawable(R.drawable.zaru_soba)));
+        recipeList.add(new Recipe("Chicken Enchiladas", getContext().getDrawable(R.drawable.chicken_enchiladas)));
     }
 }
